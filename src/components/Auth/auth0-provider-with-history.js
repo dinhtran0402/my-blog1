df10b88af1c71cd5e20e2a13bf717f2954f16b62
unknown <dinhtk223@gmail.com>
2021-07-11 00:34:46 +0700

    first commit

--- src/components/Auth/auth0-provider-with-history.js
+++ src/components/Auth/auth0-provider-with-history.js
@@ -6,12 +6,11 @@ const Auth0ProviderWithHistory = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.origin}
+      redirectUrl={window.location.origin}
     >
       {children}
     </Auth0Provider>
